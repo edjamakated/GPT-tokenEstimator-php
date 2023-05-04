07654a5a5f39a2573bf31f35bd30fe5c8765c77b
Webhosting That's Fun <rdecker@gmail.com>
2023-05-03 19:35:25 -0700

    Update tiktoken.php

--- tiktoken.php
+++ tiktoken.php
@@ -1,4 +1,162 @@
-a rough draft of a tiktoken implementation in pure PHP
+<?php
 
-i don't think this works yet but its an outline for a functional equivalent of the python tiktoken library recommended by openai for estimating tokens.
+class Encoding {
+    private $name;
+    private $pat_str;
+    private $mergeable_ranks;
+    private $special_tokens;
+    private $explicit_n_vocab;
+    private $max_token_value;
 
+    public function __construct($name, $pat_str, $mergeable_ranks, $special_tokens, $explicit_n_vocab=null) {
+        $this->name = $name;
+        $this->pat_str = $pat_str;
+        $this->mergeable_ranks = $mergeable_ranks;
+        $this->special_tokens = $special_tokens;
+
+        $this->max_token_value = max(array_merge(array_values($mergeable_ranks), array_values($special_tokens)));
+        if($explicit_n_vocab != null) {
+            if(count($mergeable_ranks) + count($special_tokens) !== $explicit_n_vocab) {
+                throw new Exception("Vocabulary counts do not match.");
+            }
+            if($this->max_token_value !== $explicit_n_vocab - 1) {
+                throw new Exception("Max token value does not match.");
+            }
+        }
+    }
+
+    public function encode_ordinary($text) {
+        // Fill in the logic to encode ordinary text
+    }
+
+    public function encode($text, $allowed_special=array(), $disallowed_special="all") {
+        // Fill in the logic to encode text with special tokens
+    }
+
+    public function encode_ordinary_batch($text_list, $num_threads=8) {
+        // Fill in the logic to encode ordinary text in a batch
+    }
+
+    public function encode_batch($text_list, $num_threads=8, $allowed_special=array(), $disallowed_special="all") {
+        // Fill in the logic to encode text in a batch, including special tokens
+    }
+
+    public function decode_bytes($tokens) {
+        // Fill in the logic to decode bytes
+    }
+
+    public function decode($tokens, $errors="replace") {
+        // Fill in the logic to decode tokens
+    }
+
+    // Other methods and functionality go here
+}
+?>
+<?php
+function read_file($blobpath) {
+    if(!substr($blobpath, 0, 7) === "http://" && !substr($blobpath, 0, 8) === "https://") {
+        // Handle local files
+        return file_get_contents($blobpath);
+    }
+    // Avoiding blobfile for public files helps avoid auth issues, like MFA prompts
+    $resp = file_get_contents($blobpath);
+    return $resp;
+}
+
+function read_file_cached($blobpath) {
+    $cache_dir = getenv("TIKTOKEN_CACHE_DIR") ?: (getenv("DATA_GYM_CACHE_DIR") ?: sys_get_temp_dir().'/data-gym-cache');
+
+    if($cache_dir === "") {
+        // disable caching
+        return read_file($blobpath);
+    }
+
+    $cache_key = sha1($blobpath);
+    $cache_path = $cache_dir.'/'.$cache_key;
+    
+    if(file_exists($cache_path)) {
+        return file_get_contents($cache_path);
+    }
+
+    $contents = read_file($blobpath);
+    if(!is_dir($cache_dir)) {
+        mkdir($cache_dir);
+    }
+    
+    $tmp_filename = $cache_path.'.'.uniqid().'.tmp';
+    
+    file_put_contents($tmp_filename, $contents);
+    rename($tmp_filename, $cache_path);
+
+    return $contents;
+}
+
+function data_gym_to_mergeable_bpe_ranks($vocab_bpe_file, $encoder_json_file) {
+    // Fill in the logic to convert data_gym_byte_to_byte and other necessary functionality.
+}
+
+function dump_tiktoken_bpe($bpe_ranks, $tiktoken_bpe_file) {
+    // Fill in the logic to dump tiktoken_bpe.
+}
+
+function load_tiktoken_bpe($tiktoken_bpe_file) {
+    // Fill in the logic to load tiktoken_bpe
+}
+
+?>
+<?php
+class Registry {
+    private static $lock;
+    private static $encodings;
+    private static $encodingConstructors;
+
+    public static function getEncoding($encodingName) {
+        if(isset(self::$encodings[$encodingName])) {
+            return self::$encodings[$encodingName];
+        }
+
+        // Fill in your encoding constructors here.
+        // Since there's no direct equivalent for Python package management, you might need to adapt the way you provide constructors.
+        $findConstructors = array(
+            "your_encoding_name" => function() {
+                return array(
+                    "key1" => "value1",
+                    "key2" => "value2"
+                );
+            },
+        );
+
+        if(!isset(self::$encodingConstructors)) {
+            self::$encodingConstructors = $findConstructors;
+        }
+
+        if(!array_key_exists($encodingName, self::$encodingConstructors)) {
+            throw new Exception("Unknown encoding ".$encodingName);
+        }
+
+        $constructor = self::$encodingConstructors[$encodingName];
+        $enc = new Encoding($constructor());
+        self::$encodings[$encodingName] = $enc;
+        return $enc;
+    }
+
+    public static function listEncodingNames() {
+        if(!isset(self::$encodingConstructors)) {
+            // Initializing constructors if not set
+        }
+        return array_keys(self::$encodingConstructors);
+    }
+}
+
+class Encoding {
+    private $name;
+
+    public function __construct($constructor) {
+        $this->name = $constructor["name"];
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+}
+?>
