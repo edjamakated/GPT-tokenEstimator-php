a8a37d129fb7d704d3252bf09e064a415eb94309
Webhosting That's Fun <rdecker@gmail.com>
2023-05-03 19:32:58 -0700

    Create tiktoken.php

---
+++ tiktoken.php
@@ -0,0 +1,4 @@
+a rough draft of a tiktoken implementation in pure PHP
+
+i don't think this works yet but its an outline for a functional equivalent of the python tiktoken library recommended by openai for estimating tokens.
+
